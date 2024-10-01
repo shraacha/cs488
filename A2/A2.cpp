b48079b3a4a3736331a6c83eff5089df6d1c02c7
Anish <racharlaanish@gmail.com>
2024-10-01 15:13:12 -0400

    reformatting
    Moved some helpers to a new file.

--- A2/A2.cpp
+++ A2/A2.cpp
@@ -1,7 +1,6 @@
 // Termm--Fall 2024
 
 #include "A2.hpp"
-#include "constants.hpp"
 #include "cs488-framework/GlErrorCheck.hpp"
 
 #include <iostream>
@@ -20,6 +19,9 @@ using namespace std;
 #include <glm/gtx/io.hpp>
 using namespace glm;
 
+#include "constants.hpp"
+#include "matrixHelpers.hpp"
+
 
 //--------- helpers ----------
 static inline line4 transformLine(const line4 &line,
@@ -27,7 +29,7 @@ static inline line4 transformLine(const line4 &line,
     return {transformation * line.first, transformation * line.second};
 }
 
-static inline std::vector<std::optional<line4>>
+static std::vector<std::optional<line4>>
 transformLines(const std::vector<std::optional<line4>> &lines,
                const glm::mat4 &transformation) {
     std::vector<std::optional<line4>> transformedLines;
@@ -39,110 +41,6 @@ transformLines(const std::vector<std::optional<line4>> &lines,
                    });
 
     return transformedLines;
-}
-
-// Params:
-//  - translatioVector[0] - translation in the x dir, relative to to current frame
-//  - translatioVector[1] - ... y dir ...
-//  - translatioVector[2] - ... z dir ...
-static inline glm::mat4 getTranslationMatrix(const glm::vec3 & inputVec) {
-    glm::mat4 translationMatrix = glm::mat4();
-    for(int i = 0; i < 3; i ++) {
-        // glm::mat4 is column major - i.e. columns are indexed first
-        translationMatrix[3][i] = inputVec[i];
-    }
-
-    return translationMatrix;
-}
-
-// Description:
-//  - creates a rotation matrix relative to the current frame. When right
-//  multiplied by a matrix, the z-axis rotation is applied first, then y, then x.
-// Params:
-//  - inputVec[0] - radians to rotate counterclockwise around the x-axis, relative to to
-//  current frame
-//  - inputVec[1] - ... y-axis ...
-//  - inputVec[2] - ... z-axis ...
-static inline glm::mat4 getRotationMatrix(const glm::vec3 &inputVec) {
-    // glm::mat4 is column major - i.e. columns are indexed first
-    glm::mat4 xAxisRotation = glm::mat4();
-    glm::mat4 yAxisRotation = glm::mat4();
-    glm::mat4 zAxisRotation = glm::mat4();
-
-    // x
-    /*
-     *     1      0       0      0
-     *     0    cos(t) -sin(t)   0
-     *     0    sin(t)  cos(t)   0
-     *     0      0       0      1
-     */
-    xAxisRotation[1][1] = cosf(inputVec[0]);
-    xAxisRotation[1][2] = sinf(inputVec[0]);
-    xAxisRotation[2][1] = -sinf(inputVec[0]);
-    xAxisRotation[2][2] = cosf(inputVec[0]);
-
-    // y
-    /*
-     * with z up, x to the right, y faces forward, not towards us.
-     * This means that we need to flip t when compared to a rotation wrt x & y.
-     * sin is an odd f'n, cos is an even f'n
-     *
-     *   cos(t)   0   sin(t)  0
-     *     0      1     0     0
-     *  -sin(t)   0   cos(t)  0
-     *     0      0     0     1
-     */
-    yAxisRotation[0][0] = cosf(inputVec[1]);
-    yAxisRotation[0][2] = -sinf(inputVec[1]);
-    yAxisRotation[2][0] = sinf(inputVec[1]);
-    yAxisRotation[2][2] = cosf(inputVec[1]);
-
-    // z
-    /*
-     * cos(t)   -sin(t)   0   0
-     * sin(t)   cos(t)    0   0
-     *   0         0      1   0
-     *   0         0      0   1
-     */
-    zAxisRotation[0][0] = cosf(inputVec[2]);
-    zAxisRotation[0][1] = sinf(inputVec[2]);
-    zAxisRotation[1][0] = -sinf(inputVec[2]);
-    zAxisRotation[1][1] = cosf(inputVec[2]);
-
-    return xAxisRotation * yAxisRotation * zAxisRotation;
-}
-
-//  Description:
-//  - creates a scaling matrix relative to the current frame.
-// Params:
-//  - inputVec[0] - factor to scale in the x-axis
-//  - inputVec[1] - ... y-axis ...
-//  - inputVec[2] - ... z-axis ...
-static inline glm::mat4 getScaleMatrix(const glm::vec3 &inputVec) {
-    glm::mat4 scaleMatrix = glm::mat4();
-
-    // glm::mat4 is column major - i.e. columns are indexed first
-    scaleMatrix[0][0] = inputVec[0];
-    scaleMatrix[1][1] = inputVec[1];
-    scaleMatrix[2][2] = inputVec[2];
-
-    return scaleMatrix;
-}
-
-static inline glm::mat4 getPerspectiveMatrix(const float & fov) {
-    glm::mat4 perspectiveMatrix = glm::mat4();
-
-    // scale x and y based on the FOV. This controls what gets mapped to 1.
-    perspectiveMatrix[0][0] = cosf(fov / 2);
-    perspectiveMatrix[1][1] = cosf(fov / 2);
-
-    // currently not mapping z
-    // TODO add z mapping
-
-    // replace w with z
-    perspectiveMatrix[2][3] = -1; // camera is looking down -z, so we flip it
-
-    return perspectiveMatrix;
 }
 
 //  Description
@@ -158,7 +56,7 @@ static inline glm::vec4 parametricLineEquation(const glm::vec4 & A, const glm::v
 
 // the line must not be a point
 // TODO clean up this spagetti code
-static inline std::optional<line4> clip(const line4 & line, const glm::vec4 & P, const glm::vec4 & n) {
+static std::optional<line4> clip(const line4 & line, const glm::vec4 & P, const glm::vec4 & n) {
     float testA = implicitLineEquation(line.first, P, n);
     float testB = implicitLineEquation(line.second, P, n);
 
@@ -180,7 +78,7 @@ static inline std::optional<line4> clip(const line4 & line, const glm::vec4 & P,
     }
 }
 
-static inline std::optional<line4>
+static std::optional<line4>
 clip(std::optional<line4> && line,
          const std::vector<pointAndNormal> &walls)
 {
@@ -191,7 +89,7 @@ clip(std::optional<line4> && line,
     return line;
 }
 
-static inline std::vector<std::optional<line4>>
+static std::vector<std::optional<line4>>
 clip(std::vector<std::optional<line4>> &&lines,
           const std::vector<pointAndNormal> &walls)
 {
@@ -205,7 +103,7 @@ clip(std::vector<std::optional<line4>> &&lines,
     return lines;
 }
 
-static inline std::vector<std::optional<line4>>
+static std::vector<std::optional<line4>>
 convertLinesToOptionalLines(const std::vector<line4> &inputLines) {
     std::vector<std::optional<line4>> optionalLines;
 
@@ -223,7 +121,7 @@ homogenize(const glm::vec4 & point) {
     return point * 1.0f/point[3];
 }
 
-static inline std::vector<std::optional<line4>>
+static std::vector<std::optional<line4>>
 homogenize(std::vector<std::optional<line4>> &&lines)
 {
     std::for_each(lines.begin(), lines.end(),
@@ -252,15 +150,14 @@ VertexData::VertexData()
 // Constructor
 A2::A2()
     : m_currentLineColour(vec3(0.0f)),
-      m_modelCubeLines{},
+      m_modelCubeLines{convertLinesToOptionalLines(c_cubeLines)},
       m_modelGnomonLines{{c_unitLineX, c_unitLineY, c_unitLineZ}},
       m_worldGnomonLines{{c_unitLineX, c_unitLineY, c_unitLineZ}},
       m_viewRotAndTsl{glm::inverse(glm::make_mat4(c_defaultCameraToWorldMatrix))},
-      m_perspective{getPerspectiveMatrix(c_defaultFOV)},
+      m_perspective{makePerspectiveMatrix(c_defaultFOV)},
       m_nearDist{c_defaultNearDistance},
       m_farDist{c_defaultFarDistance}
 {
-    m_modelCubeLines = convertLinesToOptionalLines(c_cubeLines);
 }
 
 //----------------------------------------------------------------------------------------
---
+++ A2/matrixHelpers.hpp
@@ -0,0 +1,107 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+// Params:
+//  - translatioVector[0] - translation in the x dir, relative to to current frame
+//  - translatioVector[1] - ... y dir ...
+//  - translatioVector[2] - ... z dir ...
+inline glm::mat4 makeTranslationMatrix(const glm::vec3 & inputVec) {
+    glm::mat4 translationMatrix = glm::mat4();
+    for(int i = 0; i < 3; i ++) {
+        // glm::mat4 is column major - i.e. columns are indexed first
+        translationMatrix[3][i] = inputVec[i];
+    }
+
+    return translationMatrix;
+}
+
+// Description:
+//  - creates a rotation matrix relative to the current frame. When right
+//  multiplied by a matrix, the z-axis rotation is applied first, then y, then x.
+// Params:
+//  - inputVec[0] - radians to rotate counterclockwise around the x-axis, relative to to
+//  current frame
+//  - inputVec[1] - ... y-axis ...
+//  - inputVec[2] - ... z-axis ...
+inline glm::mat4 makeRotationMatrix(const glm::vec3 &inputVec) {
+    // glm::mat4 is column major - i.e. columns are indexed first
+    glm::mat4 xAxisRotation = glm::mat4();
+    glm::mat4 yAxisRotation = glm::mat4();
+    glm::mat4 zAxisRotation = glm::mat4();
+
+    // x
+    /*
+     *     1      0       0      0
+     *     0    cos(t) -sin(t)   0
+     *     0    sin(t)  cos(t)   0
+     *     0      0       0      1
+     */
+    xAxisRotation[1][1] = cosf(inputVec[0]);
+    xAxisRotation[1][2] = sinf(inputVec[0]);
+    xAxisRotation[2][1] = -sinf(inputVec[0]);
+    xAxisRotation[2][2] = cosf(inputVec[0]);
+
+    // y
+    /*
+     * with z up, x to the right, y faces forward, not towards us.
+     * This means that we need to flip t when compared to a rotation wrt x & y.
+     * sin is an odd f'n, cos is an even f'n
+     *
+     *   cos(t)   0   sin(t)  0
+     *     0      1     0     0
+     *  -sin(t)   0   cos(t)  0
+     *     0      0     0     1
+     */
+    yAxisRotation[0][0] = cosf(inputVec[1]);
+    yAxisRotation[0][2] = -sinf(inputVec[1]);
+    yAxisRotation[2][0] = sinf(inputVec[1]);
+    yAxisRotation[2][2] = cosf(inputVec[1]);
+
+    // z
+    /*
+     * cos(t)   -sin(t)   0   0
+     * sin(t)   cos(t)    0   0
+     *   0         0      1   0
+     *   0         0      0   1
+     */
+    zAxisRotation[0][0] = cosf(inputVec[2]);
+    zAxisRotation[0][1] = sinf(inputVec[2]);
+    zAxisRotation[1][0] = -sinf(inputVec[2]);
+    zAxisRotation[1][1] = cosf(inputVec[2]);
+
+    return xAxisRotation * yAxisRotation * zAxisRotation;
+}
+
+//  Description:
+//  - creates a scaling matrix relative to the current frame.
+// Params:
+//  - inputVec[0] - factor to scale in the x-axis
+//  - inputVec[1] - ... y-axis ...
+//  - inputVec[2] - ... z-axis ...
+inline glm::mat4 makeScaleMatrix(const glm::vec3 &inputVec) {
+    glm::mat4 scaleMatrix = glm::mat4();
+
+    // glm::mat4 is column major - i.e. columns are indexed first
+    scaleMatrix[0][0] = inputVec[0];
+    scaleMatrix[1][1] = inputVec[1];
+    scaleMatrix[2][2] = inputVec[2];
+
+    return scaleMatrix;
+}
+
+inline glm::mat4 makePerspectiveMatrix(const float & fov) {
+    glm::mat4 perspectiveMatrix = glm::mat4();
+
+    // scale x and y based on the FOV. This controls what gets mapped to 1.
+    perspectiveMatrix[0][0] = cosf(fov / 2);
+    perspectiveMatrix[1][1] = cosf(fov / 2);
+
+    // currently not mapping z
+    // TODO add z mapping
+
+    // replace w with z
+    perspectiveMatrix[2][3] = -1; // camera is looking down -z, so we flip it
+
+    return perspectiveMatrix;
+}
