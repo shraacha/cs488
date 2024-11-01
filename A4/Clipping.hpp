866c2aa9efd8913ce306607d3bd479ee71565ae6
Anish <racharlaanish@gmail.com>
2024-10-31 21:26:18 -0400

    pretty sure I fixed polygons
    There were a couple issues with my project-then-clip strategy:
    
    1. faces in 3-Space which were backwards from eye POV would not render.
    
    This isn't an issue for solid objects, but if I maybe wanted to get the
    exit point & normal, the code would not detect hits with the backface.
    
    This occured because I was supplying the clockwise normal to a pair of
    vertices for clipping, this is an issue for back faces.
    
    This was fixed by checking the sign of perp_{plane}(normal) (where the
    normal refers to the 3D normal, and the plane is the projected-onto
    plane).
    
    If the the sign was positive, then we do CCW clipping (as
    normal) otherwise we do CW clipping.
    
    2. when projecting onto the xz plane, the coordinates were flipped the
    wrong way
    
    I was going from (x, y, z) -> (x, z), I needed to go from (x, y, z)
    -> (z, x) to mainain that -y faced forwards.

--- A4/Clipping.hpp
+++ A4/Clipping.hpp
@@ -2,6 +2,8 @@
 
 #include <glm/glm.hpp>
 
+enum class VertexOrder { CCW, CW };
+
 //  Description
 //  - returns (Q - P) * n, where P is a point on the line,
 //  n is normal to the line, and Q is the point being tested.
@@ -10,8 +12,20 @@ inline double implicitLineEquation(const T & Q, const T & P, const T & n) {
     return glm::dot(Q - P, n);
 }
 
+//  Description
+//  - clips in CCW order
 inline bool isInBoundsClip(const glm::dvec2 &point, const glm::dvec2 &linePoint,
-                    const glm::dvec2 &lineNormal)
+                    const glm::dvec2 &lineNormal, const VertexOrder & vertexOrder)
 {
-    return implicitLineEquation(point, linePoint, lineNormal) >= 0;
+    bool ret;
+
+    switch (vertexOrder){
+        case(VertexOrder::CCW):
+            ret = implicitLineEquation(point, linePoint, lineNormal) >= 0;
+            break;
+        case(VertexOrder::CW):
+            ret = implicitLineEquation(point, linePoint, lineNormal) <= 0;
+    }
+
+    return ret;
 }
